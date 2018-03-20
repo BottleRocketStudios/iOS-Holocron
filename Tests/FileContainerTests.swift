49a4a6b7d559abcddc346fb2e8cb9923a5d5f0c1
Will McGinty <will.mcginty@bottlerocketstudios.com>
2018-03-20 12:56:30 -0500

    More attempted fixes for testing issue on CI

--- Tests/FileContainerTests.swift
+++ Tests/FileContainerTests.swift
@@ -33,103 +33,103 @@ class FileContainerTests: XCTestCase {
         }
     }
     
-//    func testFileContainer_storeRawBytesSuccess() {
-//        let bytes = Data(bytes: [1,2,3,4,5,6,7,8])
-//        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("testBytes"))
-//        try! file.store(bytes, with: options)
-//        
-//        let stored: Data? = try! file.retrieve(with: options)
-//        XCTAssertEqual(bytes, stored)
-//    }
-//    
-//    func testFileContainer_storeCodableSuccess() {
-//        let value = Stored(id: 3, title: "some title")
-//        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("testCodable"))
-//        try! file.store(value, with: options)
-//        
-//        let stored: Stored? = try! file.retrieve(with: options)
-//        XCTAssertEqual(stored, value)
-//    }
-//    
-//    func testFileContainer_retrieveIntermediateDataFromStoredCodable() {
-//        let value = Stored(id: 3, title: "some title")
-//        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("testCodable"))
-//        try! file.store(value, with: options)
-//        
-//        let stored: Data? = try! file.retrieve(with: options)
-//        XCTAssertNotNil(stored)
-//        XCTAssertEqual(try! stored?.defaultlyDecoded(), value)
-//    }
-//    
-//    func testFileContainer_ensureFailOnNilRetrieval() {
-//        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("shouldnotExist"))
-//        let stored: Stored? = try! file.retrieve(with: options)
-//        XCTAssertNil(stored)
-//    }
-//    
-//    func testFileContainer_manualRemoval() {
-//        let value: Stored? = Stored(id: 1, title: "title")
-//        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("toBeRemoved"))
-//        
-//        try! file.store(value, with: options)
-//        let stored: Stored? = try! file.retrieve(with: options)
-//        XCTAssertEqual(value, stored)
-//        
-//        file.removeElement(with: options)
-//        let stored2: Stored? = try! file.retrieve(with: options)
-//        XCTAssertNil(stored2)
-//    }
-//    
-//    func testFileContainer_successfulWriteToUncreatedNestedDirectory() {
-//        let value: Stored? = Stored(id: 1, title: "title")
-//        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a").appendingPathComponent("b").appendingPathComponent("c"))
-//        
-//        do {
-//            try file.store(value, with: options)
-//            let stored: Stored? = try file.retrieve(with: options)
-//            XCTAssertEqual(value, stored)
-//        } catch {
-//            XCTFail(error.localizedDescription)
-//        }
-//    }
-//    
-//    func testFileContainer_failsToWriteInNonDirectory() {
-//        let value: Stored? = Stored(id: 1, title: "title")
-//        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a"))
-//        try! file.store(value, with: options)
-//        
-//        do {
-//            let options2 = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a").appendingPathComponent("b"))
-//            try file.store(value, with: options2)
-//            
-//        } catch FileContainer.Error.noParentDirectory {
-//            XCTAssertTrue(true)
-//        } catch {
-//            XCTFail("The wrong error was thrown - should be .noParentDirectory")
-//        }
-//    }
-//    
-//    func testFileContainer_doNotOverwriteWhenSpecified() {
-//        let value: Stored? = Stored(id: 1, title: "title")
-//        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a"), overwrite: false)
-//        try! file.store(value, with: options)
-//        
-//        let value2: Stored? = Stored(id: 2, title: "new title")
-//        try! file.store(value2, with: options)
-//        
-//        let stored: Stored? = try! file.retrieve(with: options)
-//        XCTAssertEqual(stored, value)
-//    }
-//    
-//    func testFileContainer_OverwriteWhenSpecified() {
-//        let value: Stored? = Stored(id: 1, title: "title")
-//        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a"), overwrite: true /* true is default */)
-//        try! file.store(value, with: options)
-//        
-//        let value2: Stored? = Stored(id: 2, title: "new title")
-//        try! file.store(value2, with: options)
-//        
-//        let stored: Stored? = try! file.retrieve(with: options)
-//        XCTAssertEqual(stored, value2)
-//    }
+    func testFileContainer_storeRawBytesSuccess() {
+        let bytes = Data(bytes: [1,2,3,4,5,6,7,8])
+        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("testBytes"))
+        try! file.store(bytes, with: options)
+        
+        let stored: Data? = try! file.retrieve(with: options)
+        XCTAssertEqual(bytes, stored)
+    }
+    
+    func testFileContainer_storeCodableSuccess() {
+        let value = Stored(id: 3, title: "some title")
+        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("testCodable"))
+        try! file.store(value, with: options)
+        
+        let stored: Stored? = try! file.retrieve(with: options)
+        XCTAssertEqual(stored, value)
+    }
+    
+    func testFileContainer_retrieveIntermediateDataFromStoredCodable() {
+        let value = Stored(id: 3, title: "some title")
+        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("testCodable"))
+        try! file.store(value, with: options)
+        
+        let stored: Data? = try! file.retrieve(with: options)
+        XCTAssertNotNil(stored)
+        XCTAssertEqual(try! stored?.defaultlyDecoded(), value)
+    }
+    
+    func testFileContainer_ensureFailOnNilRetrieval() {
+        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("shouldnotExist"))
+        let stored: Stored? = try! file.retrieve(with: options)
+        XCTAssertNil(stored)
+    }
+    
+    func testFileContainer_manualRemoval() {
+        let value: Stored? = Stored(id: 1, title: "title")
+        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("toBeRemoved"))
+        
+        try! file.store(value, with: options)
+        let stored: Stored? = try! file.retrieve(with: options)
+        XCTAssertEqual(value, stored)
+        
+        file.removeElement(with: options)
+        let stored2: Stored? = try! file.retrieve(with: options)
+        XCTAssertNil(stored2)
+    }
+    
+    func testFileContainer_successfulWriteToUncreatedNestedDirectory() {
+        let value: Stored? = Stored(id: 1, title: "title")
+        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a").appendingPathComponent("b").appendingPathComponent("c"))
+        
+        do {
+            try file.store(value, with: options)
+            let stored: Stored? = try file.retrieve(with: options)
+            XCTAssertEqual(value, stored)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testFileContainer_failsToWriteInNonDirectory() {
+        let value: Stored? = Stored(id: 1, title: "title")
+        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a"))
+        try! file.store(value, with: options)
+        
+        do {
+            let options2 = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a").appendingPathComponent("b"))
+            try file.store(value, with: options2)
+            
+        } catch FileContainer.Error.noParentDirectory {
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("The wrong error was thrown - should be .noParentDirectory")
+        }
+    }
+    
+    func testFileContainer_doNotOverwriteWhenSpecified() {
+        let value: Stored? = Stored(id: 1, title: "title")
+        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a"), overwrite: false)
+        try! file.store(value, with: options)
+        
+        let value2: Stored? = Stored(id: 2, title: "new title")
+        try! file.store(value2, with: options)
+        
+        let stored: Stored? = try! file.retrieve(with: options)
+        XCTAssertEqual(stored, value)
+    }
+    
+    func testFileContainer_OverwriteWhenSpecified() {
+        let value: Stored? = Stored(id: 1, title: "title")
+        let options = FileContainer.StorageOptions(url: documentsDirectory.appendingPathComponent("a"), overwrite: true /* true is default */)
+        try! file.store(value, with: options)
+        
+        let value2: Stored? = Stored(id: 2, title: "new title")
+        try! file.store(value2, with: options)
+        
+        let stored: Stored? = try! file.retrieve(with: options)
+        XCTAssertEqual(stored, value2)
+    }
 }
--- Tests/KeychainContainerTests.swift
+++ Tests/KeychainContainerTests.swift
@@ -23,42 +23,42 @@ class KeychainContainerTests: XCTestCase {
         }
     }
     
-//    func testKeychainContainer_storeStringSuccess() {
-//        let value: String? = "hello world"
-//        try! keychain.store(value, with: "someKey")
-//        let stored: String? = try! keychain.retrieve(with: "someKey")
-//        XCTAssertEqual(stored, value)
-//    }
-//
-//    func testKeychainContainer_storePrimitiveSuccess() {
-//        let value: Int? = 5
-//        try! keychain.store(value, with: "someKey")
-//        let stored: Int? = try! keychain.retrieve(with: "someKey")
-//        XCTAssertEqual(stored, value)
-//    }
-//
-//    func testKeychainContainer_storeCodableSuccess() {
-//        let value: Stored? = Stored(id: 1, title: "title")
-//        try! keychain.store(value, with: "someKey")
-//        let stored: Stored? = try! keychain.retrieve(with: "someKey")
-//        XCTAssertEqual(stored, value)
-//    }
-//
-//    func testKeychainContainer_ensureFailOnNilRetrieval() {
-//        let stored: Stored? = try! keychain.retrieve(with: "otherKey")
-//        XCTAssertNil(stored)
-//    }
-//
-//    func testKeychainContainer_manualRemoval() {
-//        let value: Stored? = Stored(id: 1, title: "title")
-//        let options: KeychainContainer.StorageOptions = "someKey"
-//
-//        try! keychain.store(value, with: options)
-//        let stored: Stored? = try! keychain.retrieve(with: options)
-//        XCTAssertEqual(value, stored)
-//        
-//        keychain.removeElement(with: options)
-//        let stored2: Stored? = try! keychain.retrieve(with: options)
-//        XCTAssertNil(stored2)
-//    }
+    func testKeychainContainer_storeStringSuccess() {
+        let value: String? = "hello world"
+        try! keychain.store(value, with: "someKey")
+        let stored: String? = try! keychain.retrieve(with: "someKey")
+        XCTAssertEqual(stored, value)
+    }
+    
+    func testKeychainContainer_storePrimitiveSuccess() {
+        let value: Int? = 5
+        try! keychain.store(value, with: "someKey")
+        let stored: Int? = try! keychain.retrieve(with: "someKey")
+        XCTAssertEqual(stored, value)
+    }
+
+    func testKeychainContainer_storeCodableSuccess() {
+        let value: Stored? = Stored(id: 1, title: "title")
+        try! keychain.store(value, with: "someKey")
+        let stored: Stored? = try! keychain.retrieve(with: "someKey")
+        XCTAssertEqual(stored, value)
+    }
+    
+    func testKeychainContainer_ensureFailOnNilRetrieval() {
+        let stored: Stored? = try! keychain.retrieve(with: "otherKey")
+        XCTAssertNil(stored)
+    }
+    
+    func testKeychainContainer_manualRemoval() {
+        let value: Stored? = Stored(id: 1, title: "title")
+        let options: KeychainContainer.StorageOptions = "someKey"
+        
+        try! keychain.store(value, with: options)
+        let stored: Stored? = try! keychain.retrieve(with: options)
+        XCTAssertEqual(value, stored)
+        
+        keychain.removeElement(with: options)
+        let stored2: Stored? = try! keychain.retrieve(with: options)
+        XCTAssertNil(stored2)
+    }
 }
