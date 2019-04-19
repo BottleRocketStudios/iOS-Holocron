13c00918a39508c253b26501a6faff2505610977
Pranjal Satija <pranjal.satija@bottlerocketstudios.com>
2019-04-19 16:12:23 -0500

    add tests for read / write transformations in FileSystemStorageProvider

--- Tests/FileContainerTests.swift
+++ Tests/FileContainerTests.swift
@@ -6,8 +6,34 @@
 //  Copyright © 2018 CocoaPods. All rights reserved.
 //
 
-import XCTest
 @testable import Holocron
+import XCTest
+
+class FileSystemStorageProviderTests: XCTestCase {
+    func test_transformersAreCalled() throws {
+        let readTransformExpectation = expectation(description: "readTransformer should be called.")
+        let writeTransformExpectation = expectation(description: "writeTransformer should be called.")
+        
+        let value = Data(bytes: [0, 1, 2])
+        
+        let provider = FileSystemStorageProvider(
+            baseURL: FileManager.default.temporaryDirectory,
+            readTransformer: {(data) in
+                readTransformExpectation.fulfill()
+                return data
+            },
+            writeTransformer: {(data) in
+                writeTransformExpectation.fulfill()
+                return data
+            }
+        )
+        
+        try provider.write(value, for: #function)
+        try XCTAssertEqual(provider.value(for: #function), value)
+        waitForExpectations(timeout: 1)
+    }
+}
+
 //
 //class FileContainerTests: XCTestCase {
 //    
